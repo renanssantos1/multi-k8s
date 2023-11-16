aaf507a45494e49796cd0355cb386c0b96ded495
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2023-11-16 09:16:58 -0300

    feat: create a build docker images and k8s script set

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# Build image production
+# push to docker hub
+# create tag branch check
+# push other docker hub images version tag
+docker build -t renansantos01/multi-client:latest -t renansantos01/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t renansantos01/multi-server:latest -t renansantos01/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t renansantos01/multi-worker:latest -t renansantos01/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push renansantos01/multi-client:latest
+docker push renansantos01/multi-server:latest
+docker push renansantos01/multi-worker:latest
+
+docker push renansantos01/multi-client:$SHA
+docker push renansantos01/multi-server:$SHA
+docker push renansantos01/multi-worker:$SHA
+
+# set kubectl files 
+# set image version with git sha commit
+kubectl aply -f k8s
+kubectl set image deployments/server-deployment server=renansantos01/multi-server:$SHA
+kubectl set image deployments/client-deployment client=renansantos01/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=renansantos01/multi-worker:$SHA
