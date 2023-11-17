339a0bb0722cad418f1f7f7789842818ffb34703
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2023-11-17 09:07:23 -0300

    fix : change python version

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
-# Build image production
-# push to docker hub
-# create tag branch check
-# push other docker hub images version tag
 docker build -t renansantos01/multi-client:latest -t renansantos01/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t renansantos01/multi-server:latest -t renansantos01/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t renansantos01/multi-worker:latest -t renansantos01/multi-worker:$SHA -f ./worker/Dockerfile ./worker
@@ -14,8 +10,6 @@ docker push renansantos01/multi-client:$SHA
 docker push renansantos01/multi-server:$SHA
 docker push renansantos01/multi-worker:$SHA
 
-# set kubectl files 
-# set image version with git sha commit
 kubectl aply -f k8s
 kubectl set image deployments/server-deployment server=renansantos01/multi-server:$SHA
 kubectl set image deployments/client-deployment client=renansantos01/multi-client:$SHA
